3d7a630ebadb5bcf18dea25b5b61d97639de6352
Helen <qq222@LAPTOP-DJGNT82J>
2023-03-18 17:03:09 +0800

    file for pur

---
+++ UI_design_demo/UI_design_demo/Purchase/request order/select_BPA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_design_demo.Purchase.process_request
+{
+    public partial class select_BPA : Form
+    {
+        public select_BPA()
+        {
+            InitializeComponent();
+        }
+    }
+}
