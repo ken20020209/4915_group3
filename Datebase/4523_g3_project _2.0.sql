9dcb1c6b68663ba6bdc4c429665e73da03e75724
ken20020209 <ken20020209@gmail.com>
2023-06-13 21:48:30 +0800

    include schema

--- Datebase/4523_g3_project _2.0.sql
+++ Datebase/4523_g3_project _2.0.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `4523_g1_project_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `4523_g1_project_db`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: 4523_g1_project_db
@@ -1263,4 +1265,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-13 21:38:49
+-- Dump completed on 2023-06-13 21:48:12
