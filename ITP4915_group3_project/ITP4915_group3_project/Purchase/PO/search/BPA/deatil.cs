3ab8d8d05ef0a72b44bec025016703ae1a4278c5
ken20020209 <ken20020209@gmail.com>
2023-06-15 22:58:06 +0800

    add deatil file

---
+++ ITP4915_group3_project/ITP4915_group3_project/Purchase/PO/search/BPA/deatil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915_group3_project.Purchase.PO.search.BPA
+{
+    public partial class deatil : UserControl
+    {
+        public deatil()
+        {
+            InitializeComponent();
+        }
+
+        private void bpa_headerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bpa_headerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.purchase_dbDataSet);
+
+        }
+    }
+}
