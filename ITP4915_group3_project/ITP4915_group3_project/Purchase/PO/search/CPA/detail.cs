c25c589d285f5009c088bd8df88c9b051f1e5469
ken20020209 <ken20020209@gmail.com>
2023-06-16 22:25:44 +0800

    create detail file

---
+++ ITP4915_group3_project/ITP4915_group3_project/Purchase/PO/search/CPA/detail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915_group3_project.Purchase.PO.search.CPA
+{
+    public partial class detail : UserControl
+    {
+        public detail(Control panelContent, int header_ID)
+        {
+            InitializeComponent();
+            panelContent.Controls.Add(this);
+            this.BringToFront();
+
+
+
+        }
+        private void kryptonButtonSave_Click(object sender, EventArgs e)
+        {
+/*            this.Validate();
+            this.bpa_headerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.purchase_dbDataSet);*/
+
+            MessageBox.Show("edit success");
+        }
+    }
+}
---
+++ ITP4915_group3_project/ITP4915_group3_project/Purchase/PO/search/SPO/detail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915_group3_project.Purchase.PO.search.SPO
+{
+    public partial class detail : UserControl
+    {
+        public detail(Control panelContent, int header_ID)
+        {
+            InitializeComponent();
+            panelContent.Controls.Add(this);
+            this.BringToFront();
+/*            this.bpa_headerTableAdapter.FillBy_header_ID(this.purchase_dbDataSet.bpa_header, header_ID);
+            this.bpa_linesTableAdapter.Fill(this.purchase_dbDataSet.bpa_lines);*/
+
+
+        }
+        private void kryptonButtonSave_Click(object sender, EventArgs e)
+        {
+/*            this.Validate();
+            this.bpa_headerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.purchase_dbDataSet);
+*/
+            MessageBox.Show("edit success");
+        }
+    }
+}
