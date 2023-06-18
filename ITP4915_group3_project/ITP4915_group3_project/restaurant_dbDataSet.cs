2e27ee61b8c7104cdbc4fd3144555fac481a7e72
ken20020209 <ken20020209@gmail.com>
2023-06-18 15:44:01 +0800

    udpate

--- ITP4915_group3_project/ITP4915_group3_project/restaurant_dbDataSet.cs
+++ ITP4915_group3_project/ITP4915_group3_project/restaurant_dbDataSet.cs
@@ -4,12 +4,5 @@
 
     partial class restaurant_dbDataSet
     {
-        partial class requestOrder_restaurant_stockDataTable
-        {
-        }
-
-        partial class purchasers_requestDataTable
-        {
-        }
     }
 }
