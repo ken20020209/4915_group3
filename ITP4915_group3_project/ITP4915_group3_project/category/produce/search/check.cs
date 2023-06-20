611a63957e0ed76f4e37f3f921e6edee24b419c0
Cheuk Hin CHU <220085211@stu.vtc.edu.hk>
2023-06-20 23:36:22 +0800

    link review

--- ITP4915_group3_project/ITP4915_group3_project/category/produce/search/check.cs
+++ ITP4915_group3_project/ITP4915_group3_project/category/produce/search/check.cs
@@ -12,9 +12,18 @@ namespace ITP4915_group3_project.category.produce.search
 {
     public partial class check : UserControl
     {
+        public Control panel;
         public check(Control panel)
         {
             InitializeComponent();
+            this.panel = panel;
+            this.panel.Controls.Clear();
+            this.panel.Controls.Add(this);
+            search();
+        }
+        private void search()
+        {
+
         }
 
         private void itemBindingNavigatorSaveItem_Click(object sender, EventArgs e)
--- ITP4915_group3_project/ITP4915_group3_project/category/supplier/search/check.cs
+++ ITP4915_group3_project/ITP4915_group3_project/category/supplier/search/check.cs
@@ -12,11 +12,19 @@ namespace ITP4915_group3_project.category.supplier.search
 {
     public partial class check : UserControl
     {
+        public Control panel;
         public check(Control panel)
         {
             InitializeComponent();
+            this.panel = panel;
+            this.panel.Controls.Clear();
+            this.panel.Controls.Add(this);
+            search();
         }
+        private void search()
+        {
 
+        }
         private void supplierBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             this.Validate();
